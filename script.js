19dc147dc1ac480585b939af4e854a0266932f32
Raphaël Diop <raphael.diop@laplateforme.io>
2022-10-06 16:18:59 +0200

    Site Perso V2 (format Desktop)

---
+++ script.js
@@ -0,0 +1,96 @@
+//Smoothscroll et Selection menu
+const navLinks = [...document.querySelectorAll("nav a")];
+const sections = [...document.querySelectorAll("section")];
+
+let sectionsPosition;
+
+//Calcul de la position du haut de la section en fonction du haut du doc
+function positionCalculation() {
+    sectionsPosition = sections.map(section => section.offsetTop)
+}
+positionCalculation();
+
+navLinks.forEach(link => link.addEventListener("click", addSmoothScroll))
+
+
+function addSmoothScroll(e) {
+    const linkIndex = navLinks.indexOf(e.target);
+    console.log(linkIndex);
+    window.scrollTo({
+        top: sectionsPosition[linkIndex],
+        behavior: "smooth"
+    })
+}
+
+window.addEventListener("resize",positionCalculation)
+
+//Animation du tetx page accueil
+const textAnim = document.querySelector("#loadText");
+console.log(textAnim);
+new Typewriter(textAnim,{
+    deletespeed: 70
+})
+.changeDelay(70)
+.typeString('Hey ! Moi c\'est Raphaël Diop, ')
+.pauseFor(300)
+.typeString('<span style="color:orange;">étudiant Dev Full-Stack.</span>')
+.pauseFor(300)
+.typeString('<br>')
+.typeString('Bienvenue sur mon site !')
+.start()
+
+//Apparetion des éléments avec le scroll-listening
+const imgPres = document.querySelector(".imgPres");
+const textPres = document.querySelector(".textPres");
+const carrouselImg = document.querySelector(".carrousel_img");
+const projet = document.querySelectorAll("article");
+const nav1 = document.querySelector("#link1");
+const nav2 = document.querySelector("#link2");
+const nav3 = document.querySelector("#link3");
+console.log(nav1);
+console.log(textPres);
+
+window.addEventListener("scroll", (e) =>{
+    if(window.scrollY > 550){
+        imgPres.classList.add("reveal-visible");
+        textPres.classList.add("reveal-visible");
+        
+    }
+    if(window.scrollY > 1520){
+        carrouselImg.classList.add("carrouselSlide");
+    }
+    if(window.scrollY > 2100){
+        projet.forEach(projet => {
+            projet.classList.add("reveal-visibleArt");
+        });
+    }
+})
+
+//Carrousel de compétences
+const carrouselImages = document.querySelector(".carrousel_img");
+const carrouselButtons = document.querySelectorAll("button");
+const numberOfImages = document.querySelectorAll(".carrousel_img img").length;
+let imageIndex = 1;
+let translateX = 0;
+
+
+carrouselButtons.forEach(button => {
+    button.addEventListener("click", event =>{
+        if(event.target.id === 'precedent'){
+            if(imageIndex !== 1) {
+                imageIndex--;
+                translateX += 600
+            }
+        } else{
+            if(imageIndex !== numberOfImages){
+                imageIndex++;
+                translateX -= 600;
+            }
+        }
+    
+        carrouselImages.style.transform = `translateX(${translateX}px)`;
+    })
+});
+
+   
+
